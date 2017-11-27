0bcfbc7d4defc16aaa0b6c4642839fbc80a2034b
Bence Papp <bence.papp.hu@gmail.com>
2017-11-28 00:25:29 +0100

    Exercise 20 is now done

--- week-02/day-1/Exercise20/src/Exercise20.java
+++ week-02/day-1/Exercise20/src/Exercise20.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Exercise20 {
     public static void main(String[] args) {
-        
+        // Write a program that asks for two numbers and prints the bigger one
+
+        Scanner var1 = new Scanner(System.in);
+        int userInput1 = var1.nextInt();
+
+        Scanner var2 = new Scanner(System.in);
+        int userInput2 = var2.nextInt();
+
+        if (userInput1>userInput2) {
+            System.out.println(userInput1);
+        }
+        else {
+            System.out.println(userInput2);
+        }
     }
 }
