294cce56084b4a8cb649cb0d57414b531a7e7cff
Bence Papp <bence.papp.hu@gmail.com>
2017-11-27 17:28:24 +0100

    Exercise 14 finished

--- week-02/day-1/Exercise14/src/Exercise14.java
+++ week-02/day-1/Exercise14/src/Exercise14.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Exercise14 {
     public static void main(String[] args) {
-        
+        System.out.println("Give your name!");
+        Scanner scanner = new Scanner(System.in);
+        String userInput1 = scanner.nextLine();
+        System.out.println("Your name is " + userInput1);
     }
 }
