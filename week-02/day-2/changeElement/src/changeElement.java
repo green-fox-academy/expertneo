fc6bf3fbd8fc6c1d88c1109cadcf68aa367459b0
Bence Papp <bence.papp.hu@gmail.com>
2017-11-28 16:10:49 +0100

    feat: W2D2 Change element exercise done

---
+++ week-02/day-2/changeElement/src/changeElement.java
@@ -0,0 +1,12 @@
+public class changeElement {
+    public static void main(String[] args) {
+//        # - Create an array variable named `s`
+//        #   with the following content: `[1, 2, 3, 8, 5, 6]`
+//        # - Change the 8 to 4
+//        # - Print the fourth element
+
+        int[] s = new int[] {1, 2, 3, 8, 5, 6};
+        s[3] = 4;
+        System.out.println(s[3]);
+    }
+}
---
+++ week-02/day-2/sumelements/src/sumelements.java
@@ -0,0 +1,10 @@
+public class sumelements {
+    public static void main(String[] args) {
+        // - Create an array variable named `r`
+        //   with the following content: `[54, 23, 66, 12]`
+        // - Print the sum of the second and the third element
+
+        int[] r = new int[] {54, 23, 66, 12};
+        System.out.println((r[1] + r[2]));
+    }
+}
