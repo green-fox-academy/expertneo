42ed4d796f5866a1d75366718a4ad37fca0f5b9e
Bence Papp <bence.papp.hu@gmail.com>
2017-11-28 01:48:00 +0100

    Exercise 29 done

--- week-02/day-1/Exercise29/src/Exercise29.java
+++ week-02/day-1/Exercise29/src/Exercise29.java
@@ -1,5 +1,27 @@
 public class Exercise29 {
     public static void main(String[] args) {
-        
+        // Write a program that reads a number from the standard input, then draws a
+        // pyramid like this:
+        //
+        //
+        //    *
+        //   ***
+        //  *****
+        // *******
+        //
+        // The pyramid should have as many lines as the number was
+
+        int a = 10;
+        int b = 0;
+        while (b < a) {
+            for (int i = b; i < a; i++) {
+                System.out.print(" ");
+            }
+            for (int ii = 0; ii < (b * 2); ii++) {
+                System.out.print("*");
+            }
+            System.out.println("*");
+            b++;
+        }
     }
 }
