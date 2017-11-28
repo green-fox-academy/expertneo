af5a6c9c89cb01c2f61f4222701b6b282e4370ac
Bence Papp <bence.papp.hu@gmail.com>
2017-11-28 12:59:53 +0100

    W1-D2, exercise 2 done

--- week-02/day-2/greater/src/greater.java
+++ week-02/day-2/greater/src/greater.java
@@ -1,5 +1,17 @@
 public class greater {
     public static void main(String[] args) {
-        
+        //  From now on, create the usual class wrapper
+        //  and main method on your own.
+
+        // - Create a string variable named `al` and assign the value `Greenfox` to it
+        // - Create a function called `greet` that greets it's input parameter
+        //     - Greeting is printing e.g. `Greetings dear, Greenfox`
+        // - Greet `al`
+
+        String al = "Greenfox";
+        greet(al);
+    }
+    public static void greet(String al) {
+        System.out.println("Greetings dear, " + al);
     }
 }
