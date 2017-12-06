a2dfbf8135b03cbee5451b840145b4aecb21352c
Bence Papp <bence.papp.hu@gmail.com>
2017-12-06 11:29:10 +0100

    add: W3D3 10th rainbowBoxes exercise added

---
+++ week-03/day-3/rainbowBoxes-10/src/rainbowBoxes.java
@@ -0,0 +1,44 @@
+import javax.swing.*;
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class rainbowBoxes {
+  public static void mainDraw(Graphics graphics) {
+    // create a square drawing function that takes 2 parameters:
+    // the square size, and the fill color,
+    // and draws a square of that size and color to the center of the canvas.
+    // create a loop that fills the canvas with rainbow colored squares.
+
+    int size = 100;
+    Color color = Color.blue;
+    squareDrawer(graphics, size, color);
+  }
+
+  private static void squareDrawer(Graphics graphics, int size, Color color) {
+    graphics.setColor(color);
+    graphics.drawRect(WIDTH/2-size/2, HEIGHT/2-size/2, size, size);
+  }
+
+  //    Don't touch the code below
+  static int WIDTH = 320;
+  static int HEIGHT = 343;
+
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+
+  static class ImagePanel extends JPanel {
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+
+    }
+  }
+}
