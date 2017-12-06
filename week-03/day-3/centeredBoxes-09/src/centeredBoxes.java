8bf76295c5892af8dd94acf6810a215df197be42
Bence Papp <bence.papp.hu@gmail.com>
2017-12-06 11:17:42 +0100

    add: W3D3 09 center box exercise done

---
+++ week-03/day-3/centeredBoxes-09/src/centeredBoxes.java
@@ -0,0 +1,42 @@
+import javax.swing.*;
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class centeredBoxes {
+  public static void mainDraw(Graphics graphics) {
+    // create a square drawing function that takes 1 parameter:
+    // the square size
+    // and draws a square of that size to the center of the canvas.
+    // draw 3 squares with that function.
+
+    int size = 200;
+    squareDrawer(graphics, size);
+  }
+
+  private static void squareDrawer(Graphics graphics, int size) {
+    graphics.drawRect((WIDTH/2)-size/2, (HEIGHT/2)-size/2, size, size);
+  }
+
+  //    Don't touch the code below
+  static int WIDTH = 320;
+  static int HEIGHT = 343;
+
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+
+  static class ImagePanel extends JPanel {
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+
+    }
+  }
+}
