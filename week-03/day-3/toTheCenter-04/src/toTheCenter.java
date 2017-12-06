56f1743a8285927993c56cfeae2332ce507da61c
Bence Papp <bence.papp.hu@gmail.com>
2017-12-06 10:47:35 +0100

    add: W3D3 exercise 4 added

--- week-03/day-3/toTheCenter-04/src/toTheCenter.java
+++ week-03/day-3/toTheCenter-04/src/toTheCenter.java
@@ -1,12 +1,30 @@
 import javax.swing.*;
 import java.awt.*;
+
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
-public class diagonals {
+public class toTheCenter {
   public static void mainDraw(Graphics graphics) {
-    // draw the canvas' diagonals in green. Diagonal - atlo
+    // create a line drawing function that takes 2 parameters:
+    // the x and y coordinates of the line's starting point
+    // and draws a line from that point to the center of the canvas.
+    // draw 3 lines with that function.
 
-graphics.drawLine(0,0,WIDTH, HEIGHT);
+    int x1 = 100;
+    int y2 = 100;
+    lineDrawer(graphics, x1, y2);
+
+    x1 = 80;
+    y2 = 100;
+    lineDrawer(graphics, x1, y2);
+
+    x1 = 10;
+    y2 = 300;
+    lineDrawer(graphics, x1, y2);
+  }
+
+  public static void lineDrawer(Graphics graphics, int x, int y) {
+    graphics.drawLine(x, y, WIDTH / 2, HEIGHT / 2);
   }
 
   //    Don't touch the code below
