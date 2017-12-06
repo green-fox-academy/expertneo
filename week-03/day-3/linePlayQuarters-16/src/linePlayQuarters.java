99b532fb37d9b3e6d349dde084c1a119e4b9e2ec
Bence Papp <bence.papp.hu@gmail.com>
2017-12-06 14:30:22 +0100

    add: W3D3 exercise 16 done

---
+++ week-03/day-3/linePlayQuarters-16/src/linePlayQuarters.java
@@ -0,0 +1,47 @@
+import javax.swing.*;
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class linePlayQuarters {
+  public static void mainDraw(Graphics graphics) {
+    // reproduce this:
+    // [https://github.com/greenfox-academy/teaching-materials/blob/master/workshop/drawing/line-play/r1.png]
+
+    int x = 0;
+    int y = 0;
+
+    for (int i = WIDTH/4; i < WIDTH; i = i + WIDTH/4) {
+      graphics.setColor(Color.BLUE);
+      graphics.drawLine(i, 0, WIDTH, y+i);
+    }
+
+    for (int i = HEIGHT/4; i < HEIGHT; i = i + HEIGHT/4) {
+      graphics.setColor(Color.green);
+      graphics.drawLine(0, i, x+i, HEIGHT);
+    }
+
+  }
+
+  //    Don't touch the code below
+  static int WIDTH = 320;
+  static int HEIGHT = 343;
+
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+
+  static class ImagePanel extends JPanel {
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+
+    }
+  }
+}
