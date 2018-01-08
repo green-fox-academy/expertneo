26545bca28e527c50a6351e2eab78a1c2cc569df
Bence Papp <bence.papp.hu@gmail.com>
2018-01-08 17:28:19 +0100

    feat: Implementing random number and color

--- week-07/day1/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-07/day1/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -11,6 +11,7 @@ import java.util.concurrent.atomic.AtomicLong;
 @Controller
 public class HelloWebController {
   AtomicLong newAtomicNumber = new AtomicLong();
+  String[] fontColor = {"AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"};
 
   String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
           "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
@@ -21,15 +22,23 @@ public class HelloWebController {
   @RequestMapping("/web/greeting")
   public String greeting(Model model,
                          @RequestParam(value = "name") String content) {
-    int randomNumber = (int) (Math.random()*hellos.length);
+    int randomNumberHellos = (int) (Math.random() * hellos.length);
+    int randomNumberColor = (int) (Math.random() * fontColor.length);
+    int randomFontSize = (int) (Math.random() * 40 + 20);
+
     if (content == null) {
       model.addAttribute("hello", " Hello");
       model.addAttribute("name", " World");
       model.addAttribute("counter", newAtomicNumber.incrementAndGet());
+      model.addAttribute("fontColor", "black");
+      model.addAttribute("fontSize", "10");
+
     } else {
-      model.addAttribute("hello", hellos[randomNumber]);
+      model.addAttribute("hello", hellos[randomNumberHellos]);
       model.addAttribute("name", content);
       model.addAttribute("counter", newAtomicNumber.incrementAndGet());
+      model.addAttribute("fontColor", fontColor[randomNumberColor]);
+      model.addAttribute("fontSize", randomFontSize);
     }
     return "greeting";
   }
