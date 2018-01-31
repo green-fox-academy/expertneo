79b4fec3d86dc8bb0a1e1a777cb687a438cf957c
Bence Papp <bence.papp.hu@gmail.com>
2018-01-31 23:22:25 +0100

    retake exam about half done I hope

--- week-10/retakePallida/src/main/java/com/greenfox/pallidaretake/controllers/WebController.java
+++ week-10/retakePallida/src/main/java/com/greenfox/pallidaretake/controllers/WebController.java
@@ -1,7 +1,26 @@
 package com.greenfox.pallidaretake.controllers;
 
+import com.greenfox.pallidaretake.services.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class WebController {
+
+  @Autowired
+  ItemService itemService;
+
+  @GetMapping("/")
+  public String getIndexPage(Model model) {
+    model.addAttribute("items", itemService.getAllItems());
+    model.addAttribute("uniqueItemNames", itemService.getAllUniqueItemNames());
+    model.addAttribute("uniqueItemSizes", itemService.getAllUniqueSizes());
+    return "index";
+  }
+
+  @PostMapping("/shoppingplanner/results")
+  public String clickOnTheButton
 }
--- week-10/retakePallida/src/main/java/com/greenfox/pallidaretake/services/ItemService.java
+++ week-10/retakePallida/src/main/java/com/greenfox/pallidaretake/services/ItemService.java
@@ -31,4 +31,28 @@ public class ItemService {
   public List<Item> getAllByUnitPriceLessThan(double unitPrice) {
     return itemRepository.getAllByUnitPriceLessThan(unitPrice);
   }
+
+  public List<String> getAllUniqueItemNames() {
+    List<Item> allItems = getAllItems();
+    List<String> uniqueItemNames = new ArrayList<>();
+
+    for (int i = 0; i < allItems.size() ; i++) {
+      if (!uniqueItemNames.contains(allItems.get(i).getItemName())) {
+        uniqueItemNames.add(allItems.get(i).getItemName());
+      }
+    }
+    return uniqueItemNames;
+  }
+
+  public List<String> getAllUniqueSizes() {
+    List<Item> allItems = getAllItems();
+    List<String> uniqueItemSizes = new ArrayList<>();
+
+    for (int i = 0; i < allItems.size() ; i++) {
+      if (!uniqueItemSizes.contains(allItems.get(i).getSize())) {
+        uniqueItemSizes.add(allItems.get(i).getSize());
+      }
+    }
+    return uniqueItemSizes;
+  }
 }
